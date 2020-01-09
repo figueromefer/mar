5d8cafd5a426ab7b123926e5270d6713c7237e7b
Mario <47097516+MatProgrammerSM@users.noreply.github.com>
2020-01-09 09:27:44 -0600

    Add files via upload

---
+++ mar/Clases/XamarinMaxLength.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace mar.FastEntry
+{
+    public class XamarinMaxLength
+    {
+        public string ProcessLength(string entryText, string oldString, string newString, int? maxLength)
+        {
+            string output = entryText;
+
+            if (oldString != null)
+            {
+                if (newString.Length > oldString.Length)
+                {
+                    string hil = "";
+                    if (oldString.Length > 0)
+                        hil = entryText.Remove(0, entryText.Length - 1);
+                    else
+                        hil = newString;
+
+                    // if Entry text is longer then valid length
+                    if (entryText.Length > maxLength)
+                    {
+                        entryText = entryText.Remove(entryText.Length - 1); // remove last char
+
+                        output = entryText;
+                    }
+                }
+            }
+
+            return output;
+        }
+    }
+}
