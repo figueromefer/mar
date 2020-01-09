6dba2bdbb9fcc551089af0591752963d008cd17f
figueromefer <ffigueroa@boveda-creativa.com>
2020-01-08 19:23:47 -0600

    integración de fotos y registro

--- mar.Android/MainActivity.cs
+++ mar.Android/MainActivity.cs
@@ -6,28 +6,99 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using System.Threading.Tasks;
+using Android;
+using Plugin.CurrentActivity;
 
 namespace mar.Droid
 {
     [Activity(Label = "mar", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = false, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        protected override void OnCreate(Bundle savedInstanceState)
+        protected override async void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
-
+            await TryToGetPermissions();
             base.OnCreate(savedInstanceState);
-
+            CrossCurrentActivity.Current.Init(this, savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        #region RuntimePermissions
+
+        async Task TryToGetPermissions()
+        {
+            if ((int)Build.VERSION.SdkInt >= 23)
+            {
+                await GetPermissionsAsync();
+                return;
+            }
+
+
         }
+        const int RequestLocationId = 0;
+
+        readonly string[] PermissionsGroup =
+            {
+                            //TODO add more permissions
+                            Manifest.Permission.WriteExternalStorage,
+                            Manifest.Permission.ReadExternalStorage,
+
+             };
+
+        async Task GetPermissionsAsync()
+        {
+            const string permission = Manifest.Permission.ReadContacts;
+
+            if (CheckSelfPermission(permission) == (int)Android.Content.PM.Permission.Granted)
+            {
+                //TODO change the message to show the permissions name
+                return;
+            }
+
+            if (ShouldShowRequestPermissionRationale(permission))
+            {
+                //set alert for executing the task
+                AlertDialog.Builder alert = new AlertDialog.Builder(this);
+                alert.SetTitle("Permisos");
+                alert.SetMessage("La aplicación necesita tu autorización para continuar.");
+                alert.SetPositiveButton("Solicitar permisos", (senderAlert, args) =>
+                {
+                    RequestPermissions(PermissionsGroup, RequestLocationId);
+                });
+
+                alert.SetNegativeButton("Cancelar", (senderAlert, args) =>
+                {
+                    Toast.MakeText(this, "Cancelados!", ToastLength.Short).Show();
+                });
+
+                Dialog dialog = alert.Create();
+                dialog.Show();
+
+
+                return;
+            }
+
+            RequestPermissions(PermissionsGroup, RequestLocationId);
+
+        }
+        public override async void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            switch (requestCode)
+            {
+                case RequestLocationId:
+                    {
+                        if (grantResults[0] == (int)Android.Content.PM.Permission.Granted)
+                        { }
+                    }
+                    break;
+            }
+
+        }
+
+        #endregion
     }
 }
--- mar/Paginas/Register.xaml.cs
+++ mar/Paginas/Register.xaml.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Net;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace mar
 {
@@ -47,9 +48,122 @@ namespace mar
 
         void Siguiente_Clicked(object sender, System.EventArgs e)
         {
+            string contrasena = txtContrasena.Text;
             string nombre = txtNombre.Text;
             string mail = txtMail.Text;
             string celular = txtCelular.Text;
+
+        }
+
+        #region "tomar foto perfil"
+        public async void Tomar_foto_perfil()
+        {
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("Sin camara", "No hay una camara disponible.", "OK");
+                return;
+            }
+
+            file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+
+            });
+            if (file == null)
+                return;
+            subir_perfil();
+
+        }
+
+        public async void subir_perfil()
+        {
+            try
+            {
+                string now1 = DateTime.Now.ToString().Replace(' ', '_').Replace('/', '_').Replace(':', '_');
+                if (file == null) { await DisplayAlert("Alerta", "Falta definir una foto.", "OK"); }
+                await Upload(file, "_foto_" + now1 + ".jpg");
+
+                var foto1 = "http://ec2-18-212-22-223.compute-1.amazonaws.com/upload/_foto_" + now1 + ".jpg";
+                foto1 = WebUtility.UrlEncode(foto1);
+                foto = foto1;
+                boton2.IsEnabled = true;
+                perfil.Source = ImageSource.FromStream(() =>
+                {
+                    var stream = file.GetStreamWithImageRotatedForExternalStorage();
+                    return stream;
+                });
+                file.Dispose();
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+        }
+
+
+
+        public async Task<bool> Upload(MediaFile mediaFile, string filename)
+        {
+            try
+            {
+                byte[] bitmapData;
+                var stream = new MemoryStream();
+                mediaFile.GetStreamWithImageRotatedForExternalStorage().CopyTo(stream);
+                bitmapData = stream.ToArray();
+                var fileContent = new ByteArrayContent(bitmapData);
+
+                fileContent.Headers.ContentType = MediaTypeHeaderValue.Parse("image/jpg");
+                fileContent.Headers.ContentDisposition = new ContentDispositionHeaderValue("form-data")
+                {
+                    Name = "fileUpload",
+                    FileName = filename
+                };
+
+                string boundary = "---8393774hhy37373773";
+                MultipartFormDataContent multipartContent = new MultipartFormDataContent(boundary);
+                multipartContent.Add(fileContent);
+
+
+                HttpClient httpClient = new HttpClient();
+                HttpResponseMessage response = await httpClient.PostAsync("http://boveda-creativa.net/laporciondelmar/subir.php", multipartContent);
+                response.EnsureSuccessStatusCode();
+                if (response.IsSuccessStatusCode)
+                {
+                    string content = await response.Content.ReadAsStringAsync();
+
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public static byte[] ReadFully(Stream input)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                input.CopyTo(ms);
+                return ms.ToArray();
+            }
+        }
+        #endregion
+
+        public async Task<string> httpRequest(string url)
+        {
+            Uri uri = new Uri(url);
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+            string received;
+            using (var response = (HttpWebResponse)(await Task<WebResponse>.Factory.FromAsync(request.BeginGetResponse, request.EndGetResponse, null)))
+            {
+                using (var responseStream = response.GetResponseStream())
+                { using (var sr = new StreamReader(responseStream)) { received = await sr.ReadToEndAsync(); } }
+            }
+            return received;
         }
     }
 }
